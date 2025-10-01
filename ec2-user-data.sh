16e29584e5f6529d199380cccac910fb94145165
Elialber Lopes <elialberlopes@gmail.com>
2025-10-01 18:35:50 -0300

    chore: add SSH keys to gitignore and include deployment config files

---
+++ ec2-user-data.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Update system
+apt-get update -y
+apt-get upgrade -y
+
+# Install Docker
+curl -fsSL https://get.docker.com -o get-docker.sh
+sh get-docker.sh
+usermod -aG docker ubuntu
+
+# Install Docker Compose
+curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+
+# Install Git
+apt-get install -y git
+
+# Clone repository (you'll need to do this manually or provide repo)
+# For now, we'll just ensure Docker is running
+systemctl enable docker
+systemctl start docker
+
+echo "Docker installation complete!"
+
